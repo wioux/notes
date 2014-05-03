22693b888edd538205fe4daeeac00dd3032d4050
Peter Woo <petersnowdonwoo@gmail.com>
2014-05-03 03:48:06 -0700

    Added annotate_models gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  login_name      :string(255)      not null
+#  password_digest :string(255)      not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   def self.authenticate(name, password)
     find_by_login_name(name).try(:verify, password)
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id                :integer          not null, primary key
+#  title             :string(255)
+#  body              :text
+#  date              :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  original_id       :integer
+#  derivatives_count :integer          default(0)
+#  previous_id       :integer
+#  successor_count   :integer          default(0)
+#  original_date     :datetime
+#
+
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
