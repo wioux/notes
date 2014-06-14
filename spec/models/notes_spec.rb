068259f6dc3bb331c0b104b93fc307ba38a91a24
Peter Woo <petersnowdonwoo@gmail.com>
2014-06-14 02:45:37 -0700

    Fix more deprecation warnings

--- spec/models/notes_spec.rb
+++ spec/models/notes_spec.rb
@@ -7,17 +7,21 @@ describe Note do
       note.tags.create!(:label => 'one')
       note.tags.create!(:label => 'two')
       note.tags.create!(:label => 'three')
-      note.tag_list.should eq('one, two, three')
+      expect(note.tag_list).to eq('one, two, three')
     end
   end
 
 
   describe '#tag_list=' do
     it 'should split the argument by comma and build a tag for each item' do
-      Note.new.tap{ |n| n.tag_list = '' }.tag_list.should eq('')
-      Note.new.tap{ |n| n.tag_list = 'one' }.tag_list.should eq('one')
-      Note.new.tap{ |n| n.tag_list = 'one, two' }.tag_list.should eq('one, two')
-      Note.new.tap{ |n| n.tag_list = 'one, two, three' }.tag_list.should eq('one, two, three')
+      expect( Note.new.tap{ |n| n.tag_list = '' }.tag_list ).
+        to eq('')
+      expect( Note.new.tap{ |n| n.tag_list = 'one' }.tag_list ).
+        to eq('one')
+      expect( Note.new.tap{ |n| n.tag_list = 'one, two' }.tag_list ).
+        to eq('one, two')
+      expect( Note.new.tap{ |n| n.tag_list = 'one, two, three' }.tag_list ).
+        to eq('one, two, three')
     end
 
     it 'should override all old tags' do
@@ -26,9 +30,9 @@ describe Note do
       note.tags.create!(:label => 'two')
       note.tags.create!(:label => 'three')
       note.tag_list = 'uno, dos, tres'
-      note.tag_list.should eq('uno, dos, tres')
+      expect( note.tag_list ).to eq('uno, dos, tres')
       note.save
-      Note.find(note.id).tag_list.should eq('uno, dos, tres')
+      expect( Note.find(note.id).tag_list ).to eq('uno, dos, tres')
     end
   end
 end
