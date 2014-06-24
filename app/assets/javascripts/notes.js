597f4dacbe6d111e5db024fe1f831a7e715b37fe
Peter Woo <petersnowdonwoo@gmail.com>
2014-06-23 19:49:00 -0700

    Make the <embed> replacement invisible

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -46,8 +46,11 @@ $(document).ready(function() {
     (function() {
 	var orig = document.createElement;
 	document.createElement = function() {
-	    if (arguments[0] == 'embed')
-		return orig.call(this, 'span');
+	    if (arguments[0] == 'embed') {
+		var span = orig.call(this, 'span');
+		span.classList.add('hide-me');
+		return span;
+	    }
 	    return orig.apply(this, arguments);
 	};
     })();
