8b02d2fe76fbb9e5dd087e5b26a4c561439e1d98
Peter Woo <petersnowdonwoo@gmail.com>
2015-08-18 00:03:28 -0700

    Require everything-else last

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require_tree .
 //= require bootstrap
 //= require abcjs-rails
 //= require_tree ../../../vendor/assets/javascripts
+//= require_tree .
