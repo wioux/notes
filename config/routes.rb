8da14d2059cc3773f7a800fbc9b6b7f6b53d2e3e
Peter Woo <petersnowdonwoo@gmail.com>
2015-08-19 15:50:41 -0700

    Delete unused preview routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Notes::Application.routes.draw do
 
   resources :notes, :only => [:new, :create, :update, :show, :destroy] do
     collection do
-      post 'preview'
-      put 'preview'
       patch 'preview'
       get 'filter'
       get 'browse'
