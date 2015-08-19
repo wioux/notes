05abdb347e6ad231ee80280906a9f82e6e596fd4
Peter Woo <petersnowdonwoo@gmail.com>
2015-08-19 00:39:28 -0700

    Fix indentation in hotkeys.js

--- app/assets/javascripts/hotkeys.js
+++ app/assets/javascripts/hotkeys.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-    $(window).on('keydown.saveNote', function(e) {
-        if (!e.ctrlKey && !e.metaKey)
-            return
+  $(window).on('keydown.saveNote', function(e) {
+    if (!e.ctrlKey && !e.metaKey)
+      return
 
-        if (!event.metaKey) {
-            switch(String.fromCharCode(e.which).toLowerCase()) {
-            case 'n':
-                e.preventDefault();
-                itemCreated();
-                break;
-            case 'i':
-                e.preventDefault();
-                itemInspected();
-                break;
-            }
-            return;
-        }
+    if (!event.metaKey) {
+      switch(String.fromCharCode(e.which).toLowerCase()) {
+      case 'n':
+        e.preventDefault();
+        itemCreated();
+        break;
+      case 'i':
+        e.preventDefault();
+        itemInspected();
+        break;
+      }
+      return;
+    }
 
-        switch(String.fromCharCode(e.which).toLowerCase()) {
-        case 'e':
-            e.preventDefault();
-            itemEdited();
-            break;
-        case 's':
-            e.preventDefault();
-            itemSaved();
-            break;
-        }
-    });
+    switch(String.fromCharCode(e.which).toLowerCase()) {
+    case 'e':
+      e.preventDefault();
+      itemEdited();
+      break;
+    case 's':
+      e.preventDefault();
+      itemSaved();
+      break;
+    }
+  });
 });
