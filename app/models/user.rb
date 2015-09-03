aad8193a963bae655c2e4972f8b54c58ef8994fd
Peter Woo <petersnowdonwoo@gmail.com>
2015-09-03 13:09:45 -0700

    Code formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,19 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :password
+  before_save :compute_password_digest, :unless => proc{ |u| u.password.blank? }
+
   def self.authenticate(name, password)
     find_by_login_name(name).try(:verify, password)
   end
+
   def verify(password)
     BCrypt::Password::new(password_digest) == password
   end
 
-  attr_accessor :password
-  before_save :compute_password_digest, :unless => proc{ |u| u.password.blank? }
+  private
+
   def compute_password_digest
     self.password_digest = BCrypt::Password.create(password, :cost => 11)
   end
