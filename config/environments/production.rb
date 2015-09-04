584fd768c32e90bbd9594475faba3e307bd36758
Peter Woo <petersnowdonwoo@gmail.com>
2015-09-04 00:48:45 -0700

    Upgrade rails and other deps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,8 @@ Notes::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  # Disable Rails's static file server (Apache or nginx will already do this)
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
