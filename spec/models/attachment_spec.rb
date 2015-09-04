6c74a3c961c60dbcae3c6b9c5fd13c14f8a0559b
Peter Woo <petersnowdonwoo@gmail.com>
2015-09-03 17:36:07 -0700

    Add some specs for Attachment

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 RSpec.describe Attachment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def attachment
+    tmp = Tempfile.new(['attachment_test', '.png'])
+    upload = ActionDispatch::Http::UploadedFile.new(
+      tempfile: tmp,
+      filename: 'attachment_test.png'
+    )
+    params = { uploaded_file: upload, note_id: 1 }
+    yield Attachment.create(params, without_protection: true)
+  end
+
+  describe '#location' do
+    it "should include Rails.env" do
+      attachment{ |attach| expect(attach.location).to include(Rails.env) }
+    end
+  end
+
+  describe '#local_path' do
+    it "should be a location on the filesystem" do
+      attachment do |attach|
+        expect(File.exist?(attach.local_path)).to eq(true)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    it "should delete the file" do
+      attachment do |attach|
+        expect(attach).to receive(:delete_file)
+        attach.destroy
+      end
+    end
+  end
 end
