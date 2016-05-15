754bf64c6f8bfa369b3631ddf6806eeaddfdb1f4
Peter Woo <petersnowdonwoo@gmail.com>
2016-05-14 22:05:42 -0700

    Remove route for dead action

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Notes::Application.routes.draw do
   get '/autocomplete', :to => 'notes#autocomplete'
   get '/tags/autocomplete', :to => 'tags#autocomplete'
 
-  get 'sidebar', to: 'notes#sidebar'
-
   resources :notes do
     collection do
       get 'filter'
